674df8d75a24cbfb815e3e6e68cc7372fa1eee41
SixLa <55242479+SixLa@users.noreply.github.com>
2020-01-15 16:49:43 +0100

    API liées - manque l'affichage

--- js/index.js
+++ js/index.js
@@ -1,50 +1,17 @@
-var tableau = [];
+// Partie qui récupère l'api
+const url = "https://randomuser.me/api/?results=5&inc=gender,name,nat,email,dob,registered";
 
-function afficherListe() {
-    var i;
-    var vide = "";
-    for (i = 0; i < tableau.length; i++) {
-        vide += "<li>" + tableau[i] + "</li>";
-    }
-    document.getElementById('listedesprenoms').innerHTML = vide;
+async function getUserAsync() {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data.results[0].gender);
+    console.log(data.results[0].name.first);
+    console.log(data.results[0].nat);
+    console.log(data.results[0].email);
+    console.log(data.results[0].dob.age);
+    console.log(data.results[0].registered.date);
 }
 
-afficherListe();
+getUserAsync();
 
-//Ajouter un nom
-
-var addEle = document.getElementById("ajouter");
-
-function ajouterElement() {
-    var prenomsup = document.getElementById("texte").value;
-    tableau.push(prenomsup);
-    afficherListe()
-}
-addEle.onclick = ajouterElement;
-
-
-//Supprimer un nom
-
-var supprNom = document.getElementById("supprimer");
-
-function supprimerElement() {
-    tableau.pop();
-    afficherListe()
-}
-
-supprNom.onclick = supprimerElement;
-
-// Tests API
-
-async function getUserAsync(name) {
-    let response = await fetch("https://randomuser.me/api/?results=50&inc=gender,name,nat,email,dob,registered");
-    let data = await response.json()
-    return data;
-    console.log(data);
-}
-
-getUserAsync('yourUsernameHere')
-    .then(data => console.log(data));
-
-// Tests partie 2
-const app = document.getElementById('listedesgens');
+// Créer une boucle
